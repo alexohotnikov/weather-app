2a35452a2f9055582fcd5d5ad4ed1996b999f9b3
Aleksandr Ohotnikov <ohotnikov@uchi.ru>
2021-10-22 18:36:58 +0400

    prettier and eslint added. added simple route and components.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const App: React.FC = () => {
-    return <h1>Hello from new App!</h1>
-}
+import WeatherPage from '../pages/Weather';
+import WelcomePage from '../pages/Welcome';
+
+const App: React.FC = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
+      <Route exact path="/weather">
+        <WeatherPage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ src/pages/Weather/WeatherPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const WeatherPage = () => <h1> Hello, Weather page</h1>;
+
+export default WeatherPage;
