81dd9034d8a3bba46aec7371da841be67d3d5c92
Aleksandr Ohotnikov <ohotnikov@uchi.ru>
2021-10-22 18:38:12 +0400

    prettier wepack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-      main: './src/index.tsx',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|tsx)$/,
-          use: ['babel-loader', 'ts-loader'],
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-      filename: '[name]-[contenthash].js',
-      path: path.resolve(__dirname, 'build'),
-    },
-    devtool: 'source-map',
-    devServer: {
-      hot: true,
-      liveReload: true,
-      client: {
-        logging: 'info'
-      }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Weather Test App',
-        template: 'public/index-template.html',
-        inject: 'body'
-      })
-    ],
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
+  entry: {
+    main: './src/index.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
       },
-      minimize: true,
-      minimizer: [new TerserPlugin({
-        parallel: true
-      })],
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name]-[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    liveReload: true,
+    client: {
+      logging: 'info',
     },
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather Test App',
+      template: 'public/index-template.html',
+      inject: 'body',
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      parallel: true,
+    })],
+  },
+};
